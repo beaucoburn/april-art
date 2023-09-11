df0995b38aaa4ebc11249d19d4453ae4178782cc
beaucoburn <douglas.coburn@gmail.com>
2023-09-11 17:28:29 -0600

    Colors

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -38,7 +38,7 @@ export default async function Footer() {
           <FooterMenu menu={menu} />
         </Suspense>
       </div>
-      <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
+      <div className="border-t border-teal-200 py-6 text-sm dark:border-orange-700">
         <div className="mx-auto flex w-full max-w-7xl flex-col items-center gap-1 px-4 md:flex-row md:gap-0 md:px-4 min-[1320px]:px-0">
           <p>
             &copy; {copyrightDate} {copyrightName}
