d48dc504af173772306d4a9f288ae26afdcbf5ea
beaucoburn <douglas.coburn@gmail.com>
2023-12-04 18:59:08 -0600

    Hero Margin

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <div className="bg-scroll bg-center bg-no-repeat bg-cover bg-[url('../app/public/CoverEdit.jpg')] min-h-screen min-w-fit">
+    <div className="bg-scroll bg-center bg-no-repeat bg-cover bg-[url('../app/public/CoverEdit.jpg')] min-h-screen min-w-fit mb-8">
       <h1 className="text-center pt-80 text-9xl font-semibold tracking-wide drop-shadow-lg shadow-black-700">April Elizabeth</h1>
     </div>
   )
